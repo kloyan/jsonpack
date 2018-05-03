b938a843bb4a649bd78afa29ae554b0ce5b6e204
Kaloyan Simitchiyski <kaloyan.sim@gmail.com>
2018-05-03 23:05:14 +0300

    Add atom, list and map packing

--- lib/msgpack.ex
+++ lib/msgpack.ex
@@ -1,5 +1,6 @@
 defmodule MsgPack do
 
+    @bit_4 16
     @bit_5 32
     @bit_7 128
     @bit_8 256
@@ -14,6 +15,7 @@ defmodule MsgPack do
     def pack(nil), do: << 0xC0 >>
     def pack(false), do: << 0xC2 >>
     def pack(true), do: << 0xC3 >>
+    def pack(x) when is_atom(x), do: pack(Atom.to_string(x))
     def pack(x) when is_integer(x), do: pack_uint(x)
     def pack(x) when is_float(x), do: << 0xCA, x::float >>
     def pack(x) when is_binary(x) do
@@ -22,6 +24,8 @@ defmodule MsgPack do
             false -> pack_bin(x)
         end
     end
+    def pack(x) when is_list(x), do: pack_list(x)
+    def pack(x) when is_map(x), do: pack_map(x)
 
     defp pack_uint(x) when x < @bit_7, do: << 0::1, x::7 >>
     defp pack_uint(x) when x < @bit_8, do: << 0xCC, x::8 >>
@@ -37,4 +41,28 @@ defmodule MsgPack do
     defp pack_bin(x) when byte_size(x) < @bit_8, do: << 0xC4, byte_size(x)::8, x::binary >>
     defp pack_bin(x) when byte_size(x) < @bit_16, do: << 0xC5, byte_size(x)::16, x::binary >>
     defp pack_bin(x) when byte_size(x) < @bit_32, do: << 0xC6, byte_size(x)::32, x::binary >>
+
+    defp pack_list(x) when length(x) < @bit_4, do: << 0b1001::4, length(x)::4, pack_list(x, <<>>)::binary >>
+    defp pack_list(x) when length(x) < @bit_16, do: << 0xDC, length(x)::16, pack_list(x, <<>>)::binary >>
+    defp pack_list(x) when length(x) < @bit_32, do: << 0xDD, length(x)::32, pack_list(x, <<>>)::binary >>
+
+    defp pack_list([], acc), do: acc
+    defp pack_list([head | tail], acc), do: pack_list(tail, << acc::binary, pack(head)::binary >>)
+
+    defp pack_map(x) when map_size(x) < @bit_4 do
+        << 0b1000::4, map_size(x)::4, pack_map(Map.to_list(x), <<>>)::binary >>
+    end
+
+    defp pack_map(x) when map_size(x) < @bit_16 do
+        << 0xDE, map_size(x)::16, pack_map(Map.to_list(x), <<>>)::binary >>
+    end
+
+    defp pack_map(x) when map_size(x) < @bit_32 do
+        << 0xDF, map_size(x)::32, pack_map(Map.to_list(x), <<>>)::binary >>
+    end
+
+    defp pack_map([], acc), do: acc
+    defp pack_map([{key, value} | tail], acc) do
+        pack_map(tail, << acc::binary, pack(key)::binary, pack(value)::binary >>)
+    end
 end
