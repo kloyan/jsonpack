da1e41952c8f942acc56239de6497c662ac8f922
Kaloyan Simitchiyski <kaloyan.sim@gmail.com>
2018-05-07 20:19:25 +0300

    Add tests for convertion to msgpack

--- lib/msgpack/messagepacker.ex
+++ lib/msgpack/messagepacker.ex
@@ -16,6 +16,9 @@ defmodule MessagePacker do
   def pack(true), do: <<0xC3>>
   def pack(x) when is_atom(x), do: pack(Atom.to_string(x))
 
+  # sint
+  def pack(x) when x < 0, do: raise ArgumentError, message: "negative integers are not allowed"
+
   # uint
   def pack(x) when is_integer(x), do: pack_uint(x)
 
@@ -36,23 +39,29 @@ defmodule MessagePacker do
   # map
   def pack(x) when is_map(x), do: pack_map(x)
 
-  # helper functions
-
+  # uint helper functions
   defp pack_uint(x) when x < @bit_7, do: <<0::1, x::7>>
   defp pack_uint(x) when x < @bit_8, do: <<0xCC, x::8>>
   defp pack_uint(x) when x < @bit_16, do: <<0xCD, x::16>>
   defp pack_uint(x) when x < @bit_32, do: <<0xCE, x::32>>
   defp pack_uint(x) when x < @bit_64, do: <<0xCF, x::64>>
+  defp pack_uint(x), do: raise ArgumentError, message: "integer too large: #{x}"
 
+  # binary helper functions
   defp pack_str(x) when byte_size(x) < @bit_5, do: <<0b101::3, byte_size(x)::5, x::binary>>
   defp pack_str(x) when byte_size(x) < @bit_8, do: <<0xD9, byte_size(x)::8, x::binary>>
   defp pack_str(x) when byte_size(x) < @bit_16, do: <<0xDA, byte_size(x)::16, x::binary>>
   defp pack_str(x) when byte_size(x) < @bit_32, do: <<0xDB, byte_size(x)::32, x::binary>>
+  defp pack_str(x), do: pack_large_binary(x)
 
   defp pack_bin(x) when byte_size(x) < @bit_8, do: <<0xC4, byte_size(x)::8, x::binary>>
   defp pack_bin(x) when byte_size(x) < @bit_16, do: <<0xC5, byte_size(x)::16, x::binary>>
   defp pack_bin(x) when byte_size(x) < @bit_32, do: <<0xC6, byte_size(x)::32, x::binary>>
+  defp pack_bin(x), do: pack_large_binary(x)
 
+  defp pack_large_binary(x), do: raise ArgumentError, message: "binary too large, size is #{byte_size(x)}"
+
+  # list helper functions
   defp pack_list(x) when length(x) < @bit_4,
     do: <<0b1001::4, length(x)::4, pack_list(x, <<>>)::binary>>
 
@@ -62,9 +71,12 @@ defmodule MessagePacker do
   defp pack_list(x) when length(x) < @bit_32,
     do: <<0xDD, length(x)::32, pack_list(x, <<>>)::binary>>
 
+  defp pack_list(x), do: raise ArgumentError, message: "list too large, size is #{length(x)}"
+
   defp pack_list([], acc), do: acc
   defp pack_list([head | tail], acc), do: pack_list(tail, <<acc::binary, pack(head)::binary>>)
 
+  # map helper functions
   defp pack_map(x) when map_size(x) < @bit_4 do
     <<0b1000::4, map_size(x)::4, pack_map(Map.to_list(x), <<>>)::binary>>
   end
@@ -76,6 +88,8 @@ defmodule MessagePacker do
   defp pack_map(x) when map_size(x) < @bit_32 do
     <<0xDF, map_size(x)::32, pack_map(Map.to_list(x), <<>>)::binary>>
   end
+
+  defp pack_map(x), do: raise ArgumentError, message: "map too large, size is #{map_size(x)}"
 
   defp pack_map([], acc), do: acc
 
---
+++ test/msgpack/messagepacker_test.exs
@@ -0,0 +1,52 @@
+defmodule MessagePackerTest do
+    use ExUnit.Case
+    doctest MessagePacker
+
+    test "correctly packs 'nil'" do
+        assert MessagePacker.pack(nil) == <<0xC0>>
+    end
+
+    test "correctly packs 'true' and 'false'" do
+        assert MessagePacker.pack(true) == <<0xC3>>
+        assert MessagePacker.pack(false) == <<0xC2>>
+    end
+
+    test "correctly packs atoms as strings" do
+        msgpack_value = <<171, 104, 101, 108, 108, 111, 95, 119, 111, 114, 108, 100>>
+
+        assert MessagePacker.pack(:hello_world) == msgpack_value
+        assert MessagePacker.pack("hello_world") == msgpack_value
+    end
+
+    test "correctly packs integers" do
+        assert MessagePacker.pack(127) == <<0x7F>>
+        assert MessagePacker.pack(255) == <<0xCC, 0xFF>>
+        assert MessagePacker.pack(65_535) == <<0xCD, 0xFF, 0xFF>>
+        assert MessagePacker.pack(4_294_967_295) == <<0xCe, 0xFF, 0xFF, 0xFF, 0xFF>>
+        assert MessagePacker.pack(18_446_744_073_709_551_615) == <<0xCF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF>>
+    end
+
+    test "raise exception when invalid integer is provided" do
+        assert_raise ArgumentError, fn -> MessagePacker.pack(18_446_744_073_709_551_616) end
+        assert_raise ArgumentError, fn -> MessagePacker.pack(-10) end
+    end
+
+    test "correctly packs strings" do
+        assert MessagePacker.pack("") == <<160>>
+        assert MessagePacker.pack(" ") == <<161, 32>>
+        assert MessagePacker.pack("test") == <<164, 116, 101, 115, 116>>
+    end
+
+    test "correctly packs lists" do
+        assert MessagePacker.pack([]) == <<144>>
+        assert MessagePacker.pack(Enum.to_list 1..4) == <<148, 1, 2, 3, 4>>
+        assert MessagePacker.pack(["t", "e", "s", "t"]) == <<148, 161, 116, 161, 101, 161, 115, 161, 116>>
+    end
+
+    test "correctly packs maps" do
+        assert MessagePacker.pack(%{}) == <<128>>
+        assert MessagePacker.pack(generate_map(["a", 1, "b", 2])) == <<130, 161, 97, 1, 161, 98, 2>>
+    end
+
+    defp generate_map(lst), do: lst |> Enum.chunk(2) |> Map.new(fn [k, v] -> {k, v} end)
+end
